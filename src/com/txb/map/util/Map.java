b17628df99a2e51ecc4e1ed6aacc5bf470a1a94e
zhuimenran <2161434564@qq.com>
2019-04-28 14:05:42 +0800

    add Interface  Map

---
+++ src/com/txb/map/util/Map.java
@@ -0,0 +1,17 @@
+package com.txb.map.util;
+
+public interface Map<K,V> {
+	public void add(K key, V value);
+	
+	public V remove(K key);
+	
+	public boolean contains(K key);
+	
+	public boolean isEmpty();
+	
+	public int getSize();
+	
+	public V get(K key);
+	
+	public void set(K key, V value);
+}
