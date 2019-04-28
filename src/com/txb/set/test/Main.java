99bd60bb60739eb16f26afd9628309e2f2bf0686
zhuimenran <2161434564@qq.com>
2019-04-28 16:50:16 +0800

    test set

--- src/com/txb/set/test/Main.java
+++ src/com/txb/set/test/Main.java
@@ -1,10 +1,66 @@
 package com.txb.set.test;
 
+import java.util.ArrayList;
+
+import com.txb.set.util.BSTSet;
+import com.txb.set.util.LinkedListSet;
+import com.txb.util.FileOperation;
+
 public class Main {
 
-	public static void main(String[] args) {
-		
+	//测试LinkedListSet
+	private static void testLinkedListSet() {
+        System.out.println("pride-and-prejudice: ");
+        ArrayList<String> list = new ArrayList<>();
+        FileOperation.readFile("pride-and-prejudice.txt", list);
+        System.out.println("Total words: " + list.size());
 
+        LinkedListSet<String> bstSet = new LinkedListSet<>();
+        for (String s : list) {
+            bstSet.add(s);
+        }
+        System.out.println("Total different words: " + bstSet.getSize());
+
+        System.out.println("\n a-tale-of-two-cities: ");
+        list = new ArrayList<>();
+        FileOperation.readFile("a-tale-of-two-cities.txt", list);
+        System.out.println("Total words: " + list.size());
+
+        bstSet = new LinkedListSet<>();
+        for (String s : list) {
+            bstSet.add(s);
+        }
+        System.out.println("Total different words: " + bstSet.getSize());
+    }
+
+
+    private static void testBSTSet() {
+        System.out.println("pride-and-prejudice: ");
+        ArrayList<String> list = new ArrayList<>();
+        FileOperation.readFile("pride-and-prejudice.txt", list);
+        System.out.println("Total words: " + list.size());
+
+        BSTSet<String> bstSet = new BSTSet<>();
+        for (String s : list) {
+            bstSet.add(s);
+        }
+        System.out.println("Total different words: " + bstSet.getSize());
+
+        System.out.println("\n a-tale-of-two-cities: ");
+        list = new ArrayList<>();
+        FileOperation.readFile("a-tale-of-two-cities.txt", list);
+        System.out.println("Total words: " + list.size());
+
+        bstSet = new BSTSet<>();
+        for (String s : list) {
+            bstSet.add(s);
+        }
+        System.out.println("Total different words: " + bstSet.getSize());
+    }
+    
+	public static void main(String[] args) {
+		testLinkedListSet();
+		testBSTSet();
 	}
 
 }
--- src/com/txb/set/test/TestBSTSet.java
+++ src/com/txb/set/test/TestBSTSet.java
@@ -1,9 +1,34 @@
 package com.txb.set.test;
 
+import java.util.ArrayList;
+
+import com.txb.set.util.BSTSet;
+import com.txb.util.FileOperation;
+
 public class TestBSTSet {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		System.out.println("pride-and-prejudice: ");
+        ArrayList<String> list = new ArrayList<>();
+        FileOperation.readFile("pride-and-prejudice.txt", list);
+        System.out.println("Total words: " + list.size());
+
+        BSTSet<String> bstSet = new BSTSet<>();
+        for (String s : list) {
+            bstSet.add(s);
+        }
+        System.out.println("Total different words: " + bstSet.getSize());
+
+        System.out.println("\n a-tale-of-two-cities: ");
+        list = new ArrayList<>();
+        FileOperation.readFile("a-tale-of-two-cities.txt", list);
+        System.out.println("Total words: " + list.size());
+
+        bstSet = new BSTSet<>();
+        for (String s : list) {
+            bstSet.add(s);
+        }
+        System.out.println("Total different words: " + bstSet.getSize());
 
 	}
 
